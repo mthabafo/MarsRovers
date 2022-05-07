48fb32bfcca0255c030cfbfada1b6acc0b4051e3
Mtha Bafo <skhulu.mbe@gmail.com>
2022-05-07 10:59:14 +0200

    Added RoverActions class as well as its passing unit tests

--- MarsRovers/RoverActions.cs
+++ MarsRovers/RoverActions.cs
@@ -4,21 +4,84 @@ using System.Text;
 
 namespace MarsRovers
 {
+    /// <summary>
+    /// Defines the actions of a rover, every rover must be able to perform these actions
+    /// </summary>
     public class RoverActions : IRoverActions
     {
-        public void MoveForward(Rover rover)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Turns a rover 90 degrees left
+        /// </summary>
+        /// <param name="rover">Rover object</param>
         public void RotateLeft(Rover rover)
         {
-            throw new NotImplementedException();
+            switch (rover.Direction)
+            {
+                case "N":
+                    rover.Direction = "W";
+                    break;
+                case "E":
+                    rover.Direction = "N";
+                    break;
+                case "S":
+                    rover.Direction = "E";
+                    break;
+                case "W":
+                    rover.Direction = "S";
+                    break;
+                default:
+                    throw new ArgumentException("The argument " + rover.Direction + " is not a valid direction");
+            }
         }
 
+        /// <summary>
+        /// Turns a rover 90 degrees right
+        /// </summary>
+        /// <param name="rover">Rover object</param>
         public void RotateRight(Rover rover)
         {
-            throw new NotImplementedException();
+            switch (rover.Direction)
+            {
+                case "N":
+                    rover.Direction = "E";
+                    break;
+                case "E":
+                    rover.Direction = "S";
+                    break;
+                case "S":
+                    rover.Direction = "W";
+                    break;
+                case "W":
+                    rover.Direction = "N";
+                    break;
+                default:
+                    throw new ArgumentException("The argument " + rover.Direction + " is not a valid direction");
+            }
+        }
+
+        /// <summary>
+        /// Moves a rover a rover to the next grid it is facing
+        /// </summary>
+        /// <param name="rover">Rover object</param>
+        public void MoveForward(Rover rover)
+        {
+            switch (rover.Direction)
+            {
+                case "N":
+                    rover.Y += 1;
+                    break;
+                case "E":
+                    rover.X += 1;
+                    break;
+                case "S":
+                    rover.Y -= 1;
+                    break;
+                case "W":
+                    rover.X -= 1;
+                    break;
+                default:
+                    throw new ArgumentException("The argument " + rover.Direction + " is not a valid direction");
+            }
         }
     }
 }
--- MarsRoversTests/RoverActionTest.cs
+++ MarsRoversTests/RoverActionTest.cs
@@ -1,3 +1,4 @@
+using MarsRovers;
 using System;
 using Xunit;
 
@@ -5,22 +6,52 @@ namespace MarsRoversTests
 {
     public class RoverActionTest
     {
+        private Rover _rover = new Rover();
+        private MarsRovers.RoverActions _roverAction = new MarsRovers.RoverActions();
+
         [Fact]
         public void RotateLeft()
         {
+            // arrange
+            _rover.X = 1;
+            _rover.Y = 2;
+            _rover.Direction = "N";
 
+            // act           
+            _roverAction.RotateLeft(_rover);
+
+            // assert
+            Assert.Equal("W", _rover.Direction);
         }
 
         [Fact]
         public void RotateRight()
         {
+            // arrange
+            _rover.X = 1;
+            _rover.Y = 2;
+            _rover.Direction = "N";
 
+            // act
+            _roverAction.RotateRight(_rover);
+
+            // assert
+            Assert.Equal("E", _rover.Direction);
         }
 
         [Fact]
         public void MoveForward()
         {
+            // arrange
+            _rover.X = 1;
+            _rover.Y = 2;
+            _rover.Direction = "N";
 
+            // act
+            _roverAction.MoveForward(_rover);
+
+            // assert
+            Assert.Equal(3, _rover.Y);
         }
     }
 }
